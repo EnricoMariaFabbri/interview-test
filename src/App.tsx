0fdf6a8bfba2e34ed4f4ee391c8c3d5797d6fef4
Enrico Fabbri <enry969696@gmail.com>
2022-10-13 17:12:05 +0200

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import MainPage from "./components/MainPage";
--- src/__test__/searchBar.test.tsx
+++ src/__test__/searchBar.test.tsx
@@ -1,1 +1,21 @@
-export default function test() {}
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import SearchBar from "../components/SearchBar";
+import MainPage from "../components/MainPage";
+
+test("loads and displays Input Text", async () => {
+  // ARRANGE
+  // ACT
+  // await userEvent.click(screen.getByText('Load Greeting'))
+  // await screen.findByRole('heading')
+  // // ASSERT
+  // expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+  // expect(screen.getByRole('button')).toBeDisabled()
+  render(<MainPage></MainPage>);
+  let searchBar = screen.getByPlaceholderText("Search...");
+  await userEvent.type(searchBar, "Test");
+  console.log(searchBar);
+
+  expect(searchBar).toHaveTextContent("Test");
+});
